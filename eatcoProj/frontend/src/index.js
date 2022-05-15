80e219337cadb8d0b45c9f2b467d7497243d42e5
alyssadsouza <alyssa.dsouza63@gmail.com>
2022-05-15 04:10:52 -0400

    routed login and register pages

--- eatcoProj/frontend/src/index.js
+++ eatcoProj/frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './app/App';
+import Routing from './Routing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Routing />
   </React.StrictMode>
 );
