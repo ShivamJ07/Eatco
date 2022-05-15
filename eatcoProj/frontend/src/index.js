2ba9b3ae290f1574df5cbb37184925af5799bafc
alyssadsouza <alyssa.dsouza63@gmail.com>
2022-05-15 10:04:30 -0400

    update login stuff

--- eatcoProj/frontend/src/index.js
+++ eatcoProj/frontend/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import Routing from './Routing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+localStorage.setItem('loggedIn', false);
+localStorage.setItem("user", "Alyssa");
 root.render(
   <React.StrictMode>
     <Routing />
